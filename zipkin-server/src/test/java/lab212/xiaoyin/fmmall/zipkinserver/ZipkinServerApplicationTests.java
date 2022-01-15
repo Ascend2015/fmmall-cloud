96853f88f48ebfcab1b8315e3ede0857f8102a5a
Ascend2015 <841387586@qq.com>
2022-01-15 16:23:01 +0800

    配置Spring-admin对微服务应用进行监控

--- zipkin-server/src/test/java/lab212/xiaoyin/fmmall/zipkinserver/ZipkinServerApplicationTests.java
+++ zipkin-server/src/test/java/lab212/xiaoyin/fmmall/zipkinserver/ZipkinServerApplicationTests.java
@@ -1,13 +1,9 @@
 package lab212.xiaoyin.fmmall.zipkinserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ZipkinServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
